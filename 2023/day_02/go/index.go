81e4f720c2bbc3b8f9d6f094298612e16b13a1ea
Alexandre Manoel da Silva <alemanoelsilva@protonmail.com>
2024-05-19 00:47:39 +0100

    day 2 part 1 improve code readability

--- 2023/day_02/go/index.go
+++ 2023/day_02/go/index.go
@@ -9,15 +9,37 @@ import (
 	"time"
 )
 
-func getTotalGamesId(contentInString string) int {
-	sum := 0
+const RED = "red"
+const GREEN = "green"
+const BLUE = "blue"
 
-	lines := strings.Split(contentInString, "\n")
+const MAX_RED = 12
+const MAX_GREEN = 13
+const MAX_BLUE = 14
+
+const BREAK_SETS = ";"
+const BREAK_CUBES = ","
+
+const TEST_FILE_NAME = "../sample_part_1.txt"
+
+func getCubeValue(cube, color string) int {
+	cubeValue := strings.Replace(cube, color, "", 1)
+	cubeNumber := strings.Trim(cubeValue, " ")
+
+	cubeInt, err := strconv.Atoi(cubeNumber)
+	if err != nil {
+		log.Panic("Cube int is not a number")
+	}
+
+	return cubeInt
+}
+
+func getTotalGamesId(lines []string) int {
+	sum := 0
 
 	// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 	for _, line := range lines {
-		l := strings.Replace(line, "Game ", "", 1)
-		lineAux := strings.Split(l, ":")
+		lineAux := strings.Split(strings.Replace(line, "Game ", "", 1), ":")
 
 		gameIndex, err := strconv.Atoi(lineAux[0])
 		if err != nil {
@@ -26,7 +48,7 @@ func getTotalGamesId(contentInString string) int {
 
 		isValidSet := true
 
-		sets := strings.Split(lineAux[1], ";")
+		sets := strings.Split(lineAux[1], BREAK_SETS)
 
 		// [
 		//	"8 green, 6 blue, 20 red";
@@ -34,7 +56,7 @@ func getTotalGamesId(contentInString string) int {
 		//	"5 green, 1 red"
 		// ]
 		for _, set := range sets {
-			cubesLine := strings.Split(set, ",")
+			cubesLine := strings.Split(set, BREAK_CUBES)
 
 			redSum := 0
 			greenSum := 0
@@ -42,49 +64,25 @@ func getTotalGamesId(contentInString string) int {
 
 			//	["8 green", "6 blue", "20 red"]
 			for _, cube := range cubesLine {
-				if strings.Contains(cube, "red") {
-					cubeValue := strings.Replace(cube, "red", "", 1)
-					cubeNumber := strings.Trim(cubeValue, " ")
-
-					cubeInt, err := strconv.Atoi(cubeNumber)
-					if err != nil {
-						log.Panic("Cube int is not a number")
-					}
-
-					redSum = redSum + cubeInt
-					if redSum > 12 {
+				if strings.Contains(cube, RED) {
+					redSum += getCubeValue(cube, RED)
+					if redSum > MAX_RED {
 						isValidSet = false
 						break
 					}
 				}
 
-				if strings.Contains(cube, "green") {
-					cubeValue := strings.Replace(cube, "green", "", 1)
-					cubeNumber := strings.Trim(cubeValue, " ")
-
-					cubeInt, err := strconv.Atoi(cubeNumber)
-					if err != nil {
-						log.Panic("Cube int is not a number")
-					}
-
-					greenSum = greenSum + cubeInt
-					if greenSum > 13 {
+				if strings.Contains(cube, GREEN) {
+					greenSum += getCubeValue(cube, GREEN)
+					if greenSum > MAX_GREEN {
 						isValidSet = false
 						break
 					}
 				}
 
-				if strings.Contains(cube, "blue") {
-					cubeValue := strings.Replace(cube, "blue", "", 1)
-					cubeNumber := strings.Trim(cubeValue, " ")
-
-					cubeInt, err := strconv.Atoi(cubeNumber)
-					if err != nil {
-						log.Panic("Cube int is not a number")
-					}
-
-					blueSum = blueSum + cubeInt
-					if blueSum > 14 {
+				if strings.Contains(cube, BLUE) {
+					blueSum += getCubeValue(cube, BLUE)
+					if blueSum > MAX_BLUE {
 						isValidSet = false
 						break
 					}
@@ -107,7 +105,7 @@ func getTotalGamesId(contentInString string) int {
 func main() {
 	start := time.Now()
 
-	filename := "../sample_part_1.txt"
+	filename := TEST_FILE_NAME
 	if len(os.Args) > 1 {
 		filename = os.Args[1]
 	} else {
@@ -129,11 +127,14 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	twoDigitNumberSumPart1 := getTotalGamesId(string(contentInBytePart1))
+	lines := strings.Split(string(contentInBytePart1), "\n")
+
+	twoDigitNumberSumPart1 := getTotalGamesId(lines)
 	// twoDigitNumberSumPart2 := getTwoDigitNumberPart2(string(contentInBytePart2))
 	fmt.Printf("What is the sum of the IDs of those games? - part 1: %v .\n", twoDigitNumberSumPart1)
 	// fmt.Printf("What is the sum of the IDs of those games? - part 2: %v .\n", twoDigitNumberSumPart2)
 
 	timeElapsed := time.Since(start)
-	fmt.Printf("MY  SOLUTION IN GO %v\n", timeElapsed)
+	fmt.Printf("MY  SOLUTION IN GO %v.\n", timeElapsed)
+	// fmt.Printf("MY  SOLUTION IN GO %v\n", timeElapsed)
 }
